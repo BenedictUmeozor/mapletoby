52ea5ad63abd765fd94b90335ee742e7b816430c
Benedict <benedictumeozor@gmail.com>
2024-07-05 09:54:51 +0100

    reduced padding for mobile

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -2,7 +2,7 @@ import Divider from "../ui-utils/Divider";
 
 const Banner = () => {
   return (
-    <div className="flex items-center justify-center gap-4 bg-custom_blue-20 py-6">
+    <div className="flex items-center justify-center gap-4 bg-custom_blue-20 py-6 max-md:py-4">
       <p className="text-shade-120 heading-5-lg font-bold">SAME DAY DELIVERY</p>
       <Divider />
       <p className=" text-custom_blue-80 heading-5-lg font-semibold">
